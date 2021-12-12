2c025e06005eaf621a51222d4cd0b8ed78de2c44
dansky7566 <dankuo7566@gmail.com>
2021-12-12 20:44:20 +0800

    FILTER以及ATTRIBUTE測試

--- MVC_WEB/Controllers/HomeController.cs
+++ MVC_WEB/Controllers/HomeController.cs
@@ -1,10 +1,12 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
+using MVC_WEB.Attribute;
 using MVC_WEB.Models;
 using System;
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace MVC_WEB.Controllers
@@ -32,6 +34,16 @@ namespace MVC_WEB.Controllers
         public IActionResult Error()
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+
+        [ExampleAttribute("dan", "001")]
+        public IActionResult TestModel()
+        {
+            Debug.WriteLine("A");
+            Thread.Sleep(5000);
+            Debug.WriteLine("B");
+            var testData = "Hello";
+            return Json(testData);
         }
     }
 }
--- MVC_WEB/Startup.cs
+++ MVC_WEB/Startup.cs
@@ -4,6 +4,7 @@ using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using MVC_WEB.Filter;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -23,12 +24,20 @@ namespace MVC_WEB
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+
+            services.AddMvc(config =>
+            {
+                config.Filters.Add(new InputFilter());
+            
+            });
+
             services.AddControllersWithViews();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
+
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
